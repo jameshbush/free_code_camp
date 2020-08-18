22e3d1c73f966feec064be25534aaef39d80aec6
jameshbush <jamesbvsh@gmail.com>
2020-08-18 10:46:38 -0400

    Refactors cipher js

--- 01_java_script_algorithms_and_data_structures/02_caesars_cipher/02_caesars_cipher.js
+++ 01_java_script_algorithms_and_data_structures/02_caesars_cipher/02_caesars_cipher.js
@@ -3,38 +3,39 @@ function rot13(str) {
     const ROT13_OFFSET = 13
     const ALPHA_COUNT = 26
 
-    let answer = ''
+    const answer = []
     for (let i = 0; i < str.length; i++) {
         if (str[i] < 'A' || 'Z' < str[i]) {
-            answer += str[i]
+            answer.push(str[i])
             continue
         }
 
-        answer += String.fromCharCode(
-            (
-                (
-                    (
-                        (
-                            str.charCodeAt(i)
-                        ) - A_CHAR_CODE
-                    ) + ROT13_OFFSET
-                ) % ALPHA_COUNT
-            ) + A_CHAR_CODE
-        )
+        let charCode = str.charCodeAt(i)
+        charCode -= A_CHAR_CODE
+        charCode += ROT13_OFFSET
+        charCode %= ALPHA_COUNT
+        charCode += A_CHAR_CODE
+
+        answer.push(String.fromCharCode(charCode))
     }
 
-    return answer
+    return answer.join('')
 }
 
 function rot13Test() {
-    assert(rot13("SERR PBQR PNZC") === "FREE CODE CAMP")
-    assert(rot13("SERR CVMMN!") === "FREE PIZZA!")
-    assert(rot13("SERR YBIR?") === "FREE LOVE?")
-    assert(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.") === "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.")
+    assert_eq(rot13("SERR PBQR PNZC"), "FREE CODE CAMP")
+    assert_eq(rot13("SERR CVMMN!"), "FREE PIZZA!")
+    assert_eq(rot13("SERR YBIR?"), "FREE LOVE?")
+    assert_eq(
+        rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT."),
+        "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG."
+    )
 }
 
-function assert(b) {
-    if(!b) {
-        throw(Error)
+function assert_eq(a, b) {
+    if (a !== b) {
+        console.log(a, '!=', b)
+        throw (Error)
     }
+    console.log(a, '==', b)
 }
