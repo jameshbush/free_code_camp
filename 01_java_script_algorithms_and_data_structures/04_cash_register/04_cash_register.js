b3c8406b408771765e836bb3ae23b144334abd1d
jameshbush <jamesbvsh@gmail.com>
2020-08-20 10:33:45 -0400

    Adds nested equality test

--- 01_java_script_algorithms_and_data_structures/04_cash_register/04_cash_register.js
+++ 01_java_script_algorithms_and_data_structures/04_cash_register/04_cash_register.js
@@ -42,24 +42,45 @@ function checkCashRegister(price, cash, cashInDrawer) {
     }
 }
 
+function checkNestedEquality(a, b) {
+    if (a['status'] != b['status']) {
+        console.log(a, b)
+        throw `${a} != ${b}`
+    }
+
+    if (a['change'].length !== b['change'].length) {
+        console.log(a, b)
+        throw `${a} != ${b}`
+    }
+
+    for (let i = 0; i < a['change'].length; i++) {
+        for (const j of [0, 1]) {
+            if (a['change'][i][j] != b['change'][i][j]) {
+                console.log(a, b)
+                throw `${a} != ${b}`
+            }
+        }
+    }
+}
+
 function checkCashRegisterTest() {
-    console.log(checkCashRegister(19.5, 20,
+    checkNestedEquality(checkCashRegister(19.5, 20,
         [['PENNY', 1.01], ['NICKEL', 2.05], ['DIME', 3.1], ['QUARTER', 4.25], ['ONE', 90], ['FIVE', 55], ['TEN', 20], ['TWENTY', 60], ['ONE HUNDRED', 100]]),
         { status: 'OPEN', change: [['QUARTER', 0.5]] })
 
-    console.log(checkCashRegister(3.26, 100,
+    checkNestedEquality(checkCashRegister(3.26, 100,
         [['PENNY', 1.01], ['NICKEL', 2.05], ['DIME', 3.1], ['QUARTER', 4.25], ['ONE', 90], ['FIVE', 55], ['TEN', 20], ['TWENTY', 60], ['ONE HUNDRED', 100]]),
         { status: 'OPEN', change: [['TWENTY', 60], ['TEN', 20], ['FIVE', 15], ['ONE', 1], ['QUARTER', 0.5], ['DIME', 0.2], ['PENNY', 0.04]] })
 
-    console.log(checkCashRegister(19.5, 20,
+    checkNestedEquality(checkCashRegister(19.5, 20,
         [['PENNY', 0.01], ['NICKEL', 0], ['DIME', 0], ['QUARTER', 0], ['ONE', 0], ['FIVE', 0], ['TEN', 0], ['TWENTY', 0], ['ONE HUNDRED', 0]]),
         { status: 'INSUFFICIENT_FUNDS', change: [] })
 
-    console.log(checkCashRegister(19.5, 20,
+    checkNestedEquality(checkCashRegister(19.5, 20,
         [['PENNY', 0.01], ['NICKEL', 0], ['DIME', 0], ['QUARTER', 0], ['ONE', 1], ['FIVE', 0], ['TEN', 0], ['TWENTY', 0], ['ONE HUNDRED', 0]]),
         { status: 'INSUFFICIENT_FUNDS', change: [] })
 
-    console.log(checkCashRegister(19.5, 20,
+    checkNestedEquality(checkCashRegister(19.5, 20,
         [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]),
         { status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]] })
 }
