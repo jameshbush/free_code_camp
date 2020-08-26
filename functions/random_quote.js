0d8336ef02106903f4b57a1fca21f4b726da3c38
jajajames <jamesbvsh@gmail.com>
2020-08-26 03:37:08 -0700

    Fixes how quotes are required

--- functions/random_quote.js
+++ functions/random_quote.js
@@ -1,13 +1,7 @@
-var fs = require("fs");
-var path = require("path");
-
-var jsonPath = path.join(__dirname, "quotes.json");
-var quotesJson = fs.readFileSync(jsonPath, "utf8");
-
 exports.handler = function (event, context) {
   try {
     console.log("queryStringParameters", event.queryStringParameters);
-    const quotes = JSON.parse(quotesJson);
+    const quotes = require("./quotes.json");
     const index = Math.floor(Math.random() * quotes.quotes.length);
     const quote = quotes.quotes[index];
 
