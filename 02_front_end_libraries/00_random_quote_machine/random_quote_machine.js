97b7800724c11108294ebd5d62c81b534895ffe6
jajajames <jamesbvsh@gmail.com>
2020-08-28 07:44:28 -0700

    Changes to netlify staging link

--- 02_front_end_libraries/00_random_quote_machine/random_quote_machine.js
+++ 02_front_end_libraries/00_random_quote_machine/random_quote_machine.js
@@ -1,5 +1,6 @@
 // const API_ENDPOINT = "http://localhost:8888/api/random_quote"; // when local
-const API_ENDPOINT = "http://172.16.43.128:8888/api/random_quote"; // TODO: check cors on deploy, may want to move FE to netlify
+// const API_ENDPOINT = "http://172.16.43.128:8888/api/random_quote"; // TODO: check cors on deploy, may want to move FE to netlify
+const API_ENDPOINT = "https://deploy-preview-27--hungry-hodgkin-fb2127.netlify.app/api/random_quote"; // Testing Netlify Deploy
 
 async function tellDadJoke() {
   fetch(API_ENDPOINT)
