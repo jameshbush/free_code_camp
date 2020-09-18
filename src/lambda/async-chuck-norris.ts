808d649ed7a311e3ff74a7c60280bed1b8bd1251
jajajames <jamesbvsh@gmail.com>
2020-09-17 22:42:30 -0400

    Adds axios for requests

--- src/lambda/async-chuck-norris.ts
+++ src/lambda/async-chuck-norris.ts
@@ -1,23 +1,24 @@
-import fetch from 'node-fetch';
-import { Context } from 'aws-lambda';
+import axios from "axios";
+import { Context } from "aws-lambda";
+
 export async function handler(event: any, context: Context) {
   try {
-    const response = await fetch('https://api.chucknorris.io/jokes/random');
-    if (!response.ok) {
-      // NOT res.status >= 200 && res.status < 300
+    const response = await axios.get("https://api.chucknorris.io/jokes/random");
+
+    if (!(response.status >= 200 && response.status < 300)) {
       return { statusCode: response.status, body: response.statusText };
     }
-    const data = await response.json();
+    const { value } = await response.data;
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ msg: data.value })
+      body: JSON.stringify({ msg: value }),
     };
   } catch (err) {
     console.log(err); // output to netlify function log
     return {
       statusCode: 500,
-      body: JSON.stringify({ msg: err.message }) // Could be a custom message or object i.e. JSON.stringify(err)
+      body: JSON.stringify({ msg: err.message }), // Could be a custom message or object i.e. JSON.stringify(err)
     };
   }
 }
