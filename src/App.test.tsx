477079f616c144209206e901e03970dbe59aa747
jajajames <jamesbvsh@gmail.com>
2020-10-06 13:35:59 -0400

    Removes unused component

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import TestDemo from "./TestDemo";
-
-test('renders learn react link', () => {
-  const { getByText } = render(<TestDemo />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
