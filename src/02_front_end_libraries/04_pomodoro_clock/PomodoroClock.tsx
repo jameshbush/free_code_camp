3b71d06181dd8f230a565205a0304f647099d33a
jajajames <jamesbvsh@gmail.com>
2020-10-06 09:46:13 -0400

    Refactor pomodoro

--- src/02_front_end_libraries/04_pomodoro_clock/PomodoroClock.tsx
+++ src/02_front_end_libraries/04_pomodoro_clock/PomodoroClock.tsx
@@ -9,6 +9,7 @@ interface IPomodoroClockState {
   breakLength: number;
   current: number;
   active: ClockType;
+
   timer: null | NodeJS.Timeout;
   timeout: number;
 }
@@ -17,9 +18,9 @@ const INITIAL_CLOCK = {
   sessionLength: 25 * 60,
   breakLength: 5 * 60,
   current: 0,
+  active: "session" as ClockType,
 
   timer: null,
-  active: "session" as ClockType,
   timeout: 1000,
 };
 
@@ -31,46 +32,48 @@ class PomodoroClock extends React.Component<null, IPomodoroClockState> {
   componentDidMount = () => initializeTestRunner("25-5-clock", "skip");
 
   // display
-
   roundMinutesDown = (seconds: number) => Math.floor(seconds / 60);
   remainderSeconds = (seconds: number) => seconds % 60;
   displayTime = () => {
-    const seconds = this.state[this.activeTotal()] - this.state.current;
+    const seconds = this.state[this.getActiveLengthKey()] - this.state.current;
     const mins = this.roundMinutesDown(seconds);
     const secs = this.remainderSeconds(seconds);
-    return `${mins < 10 ? `0${mins}` : mins}:${secs < 10 ? `0${secs}` : secs}`;
+    return [mins, secs].map((num) => (num < 10 ? `0${num}` : `${num}`)).join(":");
   };
   displayActive = () => `${this.state.active[0].toUpperCase()}${this.state.active.slice(1)}`;
-  activeTotal = () => `${this.state.active}Length` as "sessionLength" | "sessionLength";
+  getActiveLengthKey = () => `${this.state.active}Length` as "sessionLength" | "sessionLength";
 
   // buttons
-  handleReset = () => {
+  reset = () => {
     this.pauseTimer();
     this.getRewoundBell();
     this.setState(INITIAL_CLOCK);
   };
-  incrementBreak = () => this.updateTimeTotal("breakLength", 1);
-  decrementBreak = () => this.updateTimeTotal("breakLength", -1);
-  incrementSession = () => this.updateTimeTotal("sessionLength", 1);
-  decrementSession = () => this.updateTimeTotal("sessionLength", -1);
-  updateTimeTotal = (key: "breakLength" | "sessionLength", amount: 1 | -1) => {
-    const total = this.state[key] + amount * 60;
-    if (total <= 0) return;
-    if (total > 60 * 60) return;
-    this.setState(() => ({ [key as "sessionLength"]: total }));
+  incrementBreakLength = () => this.updateLength("breakLength", 1);
+  decrementBreakLength = () => this.updateLength("breakLength", -1);
+  incrementSessionLength = () => this.updateLength("sessionLength", 1);
+  decrementSessionLength = () => this.updateLength("sessionLength", -1);
+  updateLength = (key: "breakLength" | "sessionLength", amount: 1 | -1) => {
+    const length = this.state[key] + amount * 60;
+    if (length <= 0) return;
+    if (length > 60 * 60) return;
+    this.setState(() => ({ [key as "sessionLength"]: length }));
   };
+
+  // timer
   startTimer = () => {
     if (this.state.timer !== null) return;
-    const timer = setInterval(() => {
-      if (this.displayTime() === "00:00") {
-        this.getRewoundBell().play();
-        const active = this.state.active === "session" ? "break" : "session";
-        this.setState({ active, current: 0 });
-        return;
-      }
-      this.setState({ current: this.state.current + 1 });
-    }, this.state.timeout);
+    const timer = setInterval(this.advanceTimer, this.state.timeout);
     this.setState({ timer });
+  };
+  advanceTimer = () => {
+    if (this.displayTime() === "00:00") {
+      this.getRewoundBell().play();
+      const active = this.state.active === "session" ? "break" : "session";
+      this.setState({ active, current: 0 });
+      return;
+    }
+    this.setState({ current: this.state.current + 1 });
   };
   pauseTimer = () => {
     if (this.state.timer === null) return;
@@ -92,27 +95,29 @@ class PomodoroClock extends React.Component<null, IPomodoroClockState> {
   };
 
   render() {
-    const { roundMinutesDown, state, displayTime } = this;
+    const { roundMinutesDown, displayTime, displayActive, reset, toggleTimer } = this;
+    const { decrementSessionLength, incrementSessionLength, decrementBreakLength, incrementBreakLength } = this;
+    const { timer, sessionLength, breakLength } = this.state;
 
     return (
       <div className="container">
         <div id="tomato-clock">
           <h1>Pomodoro Clock</h1>
           <div className="grid">
-            <h4 id="timer-label">Current {this.displayActive()}</h4>
-            <ControlButton id="reset" text="Reset" onClick={this.handleReset} />
+            <h4 id="timer-label">Current {displayActive()}</h4>
+            <ControlButton id="reset" text="Reset" onClick={reset} />
             <h5 id="time-left">{displayTime()}</h5>
-            <ControlButton id="start_stop" text={state.timer ? "Pause" : "Start"} onClick={this.toggleTimer} />
+            <ControlButton id="start_stop" text={timer ? "Pause" : "Start"} onClick={toggleTimer} />
 
             <h4 id="session-label">Session Length</h4>
-            <ControlButton id="session-decrement" text="-" onClick={this.decrementSession} />
-            <h5 id="session-length">{roundMinutesDown(state.sessionLength)}</h5>
-            <ControlButton id="session-increment" text="+" onClick={this.incrementSession} />
+            <ControlButton id="session-decrement" text="-" onClick={decrementSessionLength} />
+            <h5 id="session-length">{roundMinutesDown(sessionLength)}</h5>
+            <ControlButton id="session-increment" text="+" onClick={incrementSessionLength} />
 
             <h4 id="break-label">Break Length</h4>
-            <ControlButton id="break-decrement" text="-" onClick={this.decrementBreak} />
-            <h5 id="break-length">{roundMinutesDown(state.breakLength)}</h5>
-            <ControlButton id="break-increment" text="+" onClick={this.incrementBreak} />
+            <ControlButton id="break-decrement" text="-" onClick={decrementBreakLength} />
+            <h5 id="break-length">{roundMinutesDown(breakLength)}</h5>
+            <ControlButton id="break-increment" text="+" onClick={incrementBreakLength} />
           </div>
 
           <audio
