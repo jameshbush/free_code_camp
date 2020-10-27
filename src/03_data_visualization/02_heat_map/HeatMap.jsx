6db76c9af60c30a0e7e3c2189bb5d8906469219b
jajajames <jamesbvsh@gmail.com>
2020-10-27 16:29:10 -0400

    visually okay but tests fail

--- src/03_data_visualization/02_heat_map/HeatMap.jsx
+++ src/03_data_visualization/02_heat_map/HeatMap.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { initializeTestRunner } from "../../utils/scripts/injectFCCTests";
 import * as d3 from "d3";
-import { scaleChromatic, mouse } from "d3";
 import "./styles.scss";
+// /home/b/Documents/free_code_camp/node_modules/@types/d3-selection/index.d.ts
 
 const TEMPERATURE_API_ENDPOINT =
   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
@@ -16,107 +16,176 @@ class HeatMap extends React.Component {
 
   async drawChart() {
     let temperatureData = await fetch(TEMPERATURE_API_ENDPOINT).then((res) => res.json());
-    const data = temperatureData.monthlyVariance;
+    const { monthlyVariance, baseTemperature } = temperatureData;
     console.log(
-      temperatureData,
-      // temperatureData.baseTemperature,
-      temperatureData.monthlyVariance[0].year,
-      temperatureData.monthlyVariance[0].month,
-      temperatureData.monthlyVariance[0].variance
+      baseTemperature,
+      monthlyVariance,
+      monthlyVariance[0].year,
+      monthlyVariance[0].month,
+      monthlyVariance[0].variance
     );
 
     // set the dimensions and margins of the graph
-    var margin = { top: 80, right: 25, bottom: 30, left: 40 };
-    var width = 450 - margin.left - margin.right;
-    var height = 450 - margin.top - margin.bottom;
-    const AXIS_FONT_SIZE = 15;
+    var margin = { top: 80, right: 25, bottom: 30, left: 65 };
+    var width = 1500 - margin.left - margin.right;
+    var height = 400 - margin.top - margin.bottom;
 
     // append the svg object to the body of the page
     var svg = d3
       .select("#heat-map-svg")
-      .append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    console.log(data);
+    // X/Y/C SCALE
+    const years = [...new Set(d3.map(monthlyVariance, (d) => d.year))]; // ( 1953 ... 2015 )
+    const variances = [...new Set(d3.map(monthlyVariance, (d) => d.variance))]; // ( -6.976 ... 5.228 )
+    // console.log(years, variances);
 
-    // Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
-    var myGroups = d3.map(data, (d) => d.month).keys();
-    var myVars = d3.map(data, (d) => d.year).keys();
+    const MONTH_NAMES = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const SCALE_BAND_PADDING = 0.01;
+    var xScale = d3
+      .scaleBand()
+      .range([0, width])
+      .domain(years)
+      .paddingInner(SCALE_BAND_PADDING)
+      .paddingOuter(SCALE_BAND_PADDING);
+    var yScale = d3
+      .scaleBand()
+      .range([height, 0])
+      .domain(MONTH_NAMES.map((_, i) => i).reverse())
+      .paddingInner(SCALE_BAND_PADDING)
+      .paddingOuter(SCALE_BAND_PADDING);
+    var cScale = d3
+      .scaleSequential()
+      .interpolator(d3.interpolate("lightgreen", "darkred"))
+      .domain([d3.min(variances), d3.max(variances)]); // http://using-d3js.com/04_05_sequential_scales.html
 
-    // Build X scales and axis:
-    var xScale = d3.scaleBand().range([0, width]).domain(myGroups).padding(0.05);
+    // X/Y AXIS
+    const AXIS_FONT_SIZE = 12;
     svg
       .append("g")
+      .attr("id", "x-axis")
       .style("font-size", AXIS_FONT_SIZE)
-      .attr("transform", "translate(0," + height + ")")
+      .attr("transform", `translate(0,${height})`)
       .call(d3.axisBottom(xScale).tickSize(0))
-      .select(".domain")
-      .remove();
+      .selectAll("text")
+      .attr("opacity", (year) => (Number.isInteger(year / 10) ? 1 : 0));
 
-    // Build Y scales and axis:
-    var yScale = d3.scaleBand().range([height, 0]).domain(myVars).padding(0.05);
     svg
       .append("g")
+      .attr("id", "y-axis")
       .style("font-size", AXIS_FONT_SIZE)
       .call(d3.axisLeft(yScale).tickSize(0))
-      .select(".domain")
-      .remove();
+      .selectAll("text")
+      .text((monthIndex) => MONTH_NAMES[monthIndex]);
 
-    // Build color scale
-    var myColor = d3.scaleSequential().interpolator(d3.interpolateInferno).domain([1, 100]);
-
-    // create a tooltip
-    // var tooltip = d3
-    //   .select("#heat-map-svg")
-    //   .append("div")
-    //   .style("opacity", 0)
-    //   .attr("class", "tooltip")
-    //   .style("background-color", "white")
-    //   .style("border", "solid")
-    //   .style("border-width", "2px")
-    //   .style("border-radius", "5px")
-    //   .style("padding", "5px");
-
-    // Three function that change the tooltip when user hover / move / leave a cell
-    // var mouseover = function (d) {
-    //   tooltip.style("opacity", 1);
-    //   d3.select(this).style("stroke", "black").style("opacity", 1);
-    // };
-    // var mousemove = function (d) {
-    //   tooltip.html("The exact value of<br>this cell is: " + d.value);
-    //   .style("left", d3.mouse(this)[0] + 70 + "px")
-    //   .style("top", d3.mouse(this)[1] + "px");
-    // };
-    // var mouseleave = function (d) {
-    //   tooltip.style("opacity", 0);
-    //   d3.select(this).style("stroke", "none").style("opacity", 0.8);
-    // };
-
-    const RADIUS = 4;
-    // add the squares
+    // Legend
+    const LEGEND_WIDTH = 10;
+    const LEGEND_DATA = [d3.min(variances), d3.mean(variances), 0, d3.max(variances)];
     svg
-      .selectAll()
-      .data(data, (d) => d.year + ":" + d.month)
+      .append("rect")
+      .attr("x", 240)
+      .attr("y", -36)
+      .attr("height", 28)
+      .attr("width", 233)
+      .attr("id", "legend");
+      // .attr("background", "blue")
+      // .attr("fill", "blue");
+
+    svg
+      .selectAll("legend-rect")
+      .append("rect")
+      .data(LEGEND_DATA)
       .enter()
       .append("rect")
+      .attr("fill", (d) => cScale(d))
+      .attr("x", (_, i) => 250 + i * (LEGEND_WIDTH + 50))
+      .attr("y", -28)
+      .attr("height", LEGEND_WIDTH)
+      .attr("width", LEGEND_WIDTH);
+
+    svg
+      .selectAll("legend-text")
+      .data(LEGEND_DATA)
+      .enter()
+      .append("text")
+      .text((d) => d.toFixed(2))
+      .attr("x", (_, i) => 265 + i * (LEGEND_WIDTH + 50))
+      .attr("y", -20)
+      .style("font-size", 12)
+      .attr("class", "bacon");
+
+    // Tooltip
+    var tooltip = d3
+      .select("#heat-map-wrapper")
+      .append("div")
+      .attr("id", "tooltip")
+      .style("opacity", 0)
+      .attr("class", "tooltip")
+      .style("background", "#EEE")
+      .style("border", "solid")
+      .style("border-width", "2px")
+      .style("border-radius", "5px")
+      .style("padding", "5px");
+
+    // Cells
+    const RADIUS = 0;
+    const JS_MONTH_OFFSET = -1;
+    svg
+      .selectAll()
+      .data(monthlyVariance)
+      .enter()
+      .append("rect")
+      .attr("class", "rectangle cell")
+      .attr("data-month", (d) => d.month + JS_MONTH_OFFSET)
+      .attr("data-year", (d) => d.year)
+      .attr("data-temp", (d) => d.variance)
       .attr("x", (d) => xScale(d.year))
-      .attr("y", (d) => yScale(d.month))
+      .attr("y", (d) => yScale(d.month + JS_MONTH_OFFSET))
       .attr("rx", RADIUS)
       .attr("ry", RADIUS)
       .attr("width", xScale.bandwidth())
       .attr("height", yScale.bandwidth())
-      .style("fill", (d) => myColor(d.variance))
+      .style("fill", (d) => cScale(d.variance))
       .style("stroke-width", RADIUS)
       .style("stroke", "none")
-      .style("opacity", 0.8);
-    // .on("mouseover", mouseover)
-    // .on("mousemove", mousemove)
-    // .on("mouseleave", mouseleave);
+      .style("opacity", 0.8)
+      .on("mouseover", function (_) {
+        tooltip.style("opacity", 1);
+        d3.select(this).style("stroke", "black").style("opacity", 1);
+      })
+      .on("mousemove", (event, data) => {
+        // console.log(event, data);
+        const [x, y] = d3.pointer(event);
+        // console.log(x, y);
+        tooltip
+          .html(`The exact value of<br>this cell is: ${data.variance}`)
+          .style("left", `${x + 80}px`)
+          .style("top", `${y + 160}px`)
+          .attr("data-year", data.year)
+          .attr("data-month", data.month);
+      })
+      .on("mouseleave", function (d) {
+        tooltip.style("opacity", 0);
+        d3.select(this).style("stroke", "none").style("opacity", 0.8);
+      });
 
-    // Add title to graph
+    // TITLE
     svg
       .append("text")
       .attr("x", 0)
@@ -125,7 +194,7 @@ class HeatMap extends React.Component {
       .style("font-size", "22px")
       .text("A d3.js Heatmap");
 
-    // Add subtitle to graph
+    // SUBTITLE
     svg
       .append("text")
       .attr("x", 0)
@@ -134,22 +203,22 @@ class HeatMap extends React.Component {
       .style("font-size", "14px")
       .style("fill", "grey")
       .style("max-width", 400)
-      .text(`Base temp: ${temperatureData.baseTemperature}`);
-
-    return;
+      .text(`Base temp: ${baseTemperature} ℃ = 0.00`);
   }
 
   render() {
     return (
-      <div className="container">
+      <div className="container-fluid">
         <div className="row">
           <div className="col-12">
             <h1 id="title">Temperature Heat Map</h1>
             <p id="description">
-              How much is the earth heating? How does seasonality influence tempuratures? This shows seven
-              decades of temperatures.
+              How much is the earth heating? How does seasonality influence tempuratures? This shows two and a
+              half centuries of monthly world temperatures.
             </p>
-            <svg id="heat-map-svg" ref={(elem) => (this.svg = elem)}></svg>
+            <div id="heat-map-wrapper">
+              <svg id="heat-map-svg" ref={(elem) => (this.svg = elem)}></svg>
+            </div>
           </div>
         </div>
       </div>
